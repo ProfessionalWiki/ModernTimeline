36c0975b417d2c360be961294f1a92b890049858
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-07-31 18:08:46 +0200

    Add extension registration

---
+++ src/ModernTimelineSetup.php
@@ -0,0 +1,28 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ModernTimeline;
+
+class ModernTimelineSetup {
+
+	public static function onExtensionFunction() {
+		self::doSmwCheck();
+	}
+
+	private static function doSmwCheck() {
+		if ( !defined( 'SMW_VERSION' ) ) {
+
+			if ( PHP_SAPI === 'cli' || PHP_SAPI === 'phpdbg' ) {
+				die( "\nThe 'Modern Timeline' extension requires 'Semantic MediaWiki' to be installed and enabled.\n" );
+			}
+
+			die(
+				'<b>Error:</b> The <a href="https://github.com/ProfessionalWiki/ModernTimeline">Modern Timeline</a> ' .
+				'extension requires <a href="https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki">Semantic MediaWiki</a> to be ' .
+				'installed and enabled.<br />'
+			);
+		}
+	}
+
+}
