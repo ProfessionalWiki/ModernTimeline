85d270bd54e68a072484beacade6aa0016b86950
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-08-02 14:14:42 +0200

    Add basic JsonBuilder test

--- tests/Unit/JsonBuilderTest.php
+++ tests/Unit/JsonBuilderTest.php
@@ -5,15 +5,41 @@ declare( strict_types = 1 );
 namespace ModernTimeline\Tests\Unit;
 
 use ModernTimeline\JsonBuilder;
+use ModernTimeline\ResultFacade\Subject;
+use ModernTimeline\ResultFacade\SubjectCollection;
 use PHPUnit\Framework\TestCase;
+use SMW\DIWikiPage;
 
 /**
  * @covers \ModernTimeline\JsonBuilder
  */
 class JsonBuilderTest extends TestCase {
 
-	public function testFoo() {
-		$this->assertTrue( class_exists( JsonBuilder::class ) );
+	public function testEmptySubjectCollection() {
+		$this->assertBuildsJson(
+			[],
+			new SubjectCollection(
+				[
+					new Subject(
+						$this->newDiWikiPage(),
+						[]
+					)
+				]
+			)
+		);
+	}
+
+	private function newDiWikiPage(): DIWikiPage {
+		return $this->createMock( DIWikiPage::class );
+	}
+
+	public function assertBuildsJson( array $expectedJson, SubjectCollection $input ) {
+		$this->assertSame(
+			[
+				'events' => $expectedJson
+			],
+			( new JsonBuilder() )->buildTimelineJson( $input )
+		);
 	}
 
 }
