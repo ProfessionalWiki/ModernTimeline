e15adf877e97b9f1db4ecacd8e235dd9c5bb8a2a
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-08-02 17:39:06 +0200

    Remove intermediate options representation

--- src/ModernTimelinePrinter.php
+++ src/ModernTimelinePrinter.php
@@ -14,85 +14,12 @@ use SMWQuery;
 
 class ModernTimelinePrinter implements ResultPrinter {
 
-	private const PARAM_WIDTH = 'width';
-	private const PARAM_HEIGHT = 'height';
-	private const PARAM_BOOKMARK = 'bookmark';
-	private const PARAM_BACKGROUND = 'background';
-	private const PARAM_SCALE_FACTOR = 'scale factor';
-	private const PARAM_POSITION = 'position';
-	private const PARAM_TICK_WIDTH = 'tick width';
-	private const PARAM_START_SLIDE = 'start slide';
-	private const PARAM_START_AT_END = 'start at end';
-
 	public function getName(): string {
 		return wfMessage( 'modern-timeline-format-name' )->text();
 	}
 
 	public function getParamDefinitions( array $definitions ) {
-		return array_merge( $definitions, $this->getTimelineParameterDefinitions() );
-	}
-
-	private function getTimelineParameterDefinitions(): array {
-		$definitions = [];
-
-		$definitions[self::PARAM_WIDTH] = [
-			'type' => 'dimension',
-			'allowauto' => true,
-			'units' => [ 'px', 'ex', 'em', '%', '' ],
-			'default' => $GLOBALS['wgModernTimelineWidth'],
-		];
-
-		$definitions[self::PARAM_HEIGHT] = [
-			'type' => 'dimension',
-			'units' => [ 'px', 'ex', 'em', '' ],
-			'default' => $GLOBALS['wgModernTimelineHeight'],
-		];
-
-		$definitions[self::PARAM_BOOKMARK] = [
-			'type' => 'boolean',
-			'default' => $GLOBALS['wgModernTimelineBookmark'],
-		];
-
-		$definitions[self::PARAM_BACKGROUND] = [
-			'type' => 'string',
-			'default' => $GLOBALS['wgModernTimelineBackground'],
-		];
-
-		$definitions[self::PARAM_SCALE_FACTOR] = [
-			'type' => 'integer',
-			'default' => $GLOBALS['wgModernTimelineScaleFactor'],
-		];
-
-		$definitions[self::PARAM_POSITION] = [
-			'type' => 'string',
-			'default' => $GLOBALS['wgModernTimelinePosition'],
-			'values' => [ 'top', 'bottom' ],
-		];
-
-		$definitions[self::PARAM_TICK_WIDTH] = [
-			'type' => 'integer',
-			'default' => $GLOBALS['wgModernTimelineTickWidth']
-		];
-
-		$definitions[self::PARAM_START_SLIDE] = [
-			'type' => 'integer',
-			'default' => $GLOBALS['wgModernTimelineStartSlide']
-		];
-
-		$definitions[self::PARAM_START_AT_END] = [
-			'type' => 'boolean',
-			'default' => $GLOBALS['wgModernTimelineStartAtEnd']
-		];
-
-		foreach ( $definitions as $name => $definition ) {
-			$definitions[$name]['message'] = 'modern-timeline-param-' . str_replace( ' ', '-', $name );
-
-			if ( strpos( $name, ' ' ) !== false ) {
-				$definitions[$name]['aliases'] = [ str_replace( ' ', '', $name ) ];
-			}
-		}
-
-		return $definitions;
+		return array_merge( $definitions, TimelineOptions::getTimelineParameterDefinitions() );
 	}
 
 	/**
@@ -109,13 +36,13 @@ class ModernTimelinePrinter implements ResultPrinter {
 
 		SMWOutputs::requireScript(
 			$timelineId,
-			$this->createJs( $timelineId, $result, $this->newOptionsFromParameters( $parameters ) )
+			$this->createJs( $timelineId, $result, $parameters )
 		);
 
 		return $this->createDiv(
 			$timelineId,
-			$parameters[self::PARAM_WIDTH]->getValue(),
-			$parameters[self::PARAM_HEIGHT]->getValue()
+			$parameters[TimelineOptions::PARAM_WIDTH]->getValue(),
+			$parameters[TimelineOptions::PARAM_HEIGHT]->getValue()
 		);
 	}
 
@@ -135,20 +62,12 @@ class ModernTimelinePrinter implements ResultPrinter {
 		);
 	}
 
-	public function createJs( string $timelineId, QueryResult $result, TimelineOptions $options ): string {
+	public function createJs( string $timelineId, QueryResult $result, array $parameters ): string {
 		$preJson = ( new JsonBuilder() )->buildTimelineJson(
 			( new ResultSimplifier() )->newSubjectCollection( $result )
 		);
 
-		$preJson['options'] = [
-			'hash_bookmark' => $options->bookmark,
-			'default_bg_color' => $options->backgroundColor,
-			'scale_factor' => $options->scaleFactor,
-			'timenav_position' => $options->position,
-			'optimal_tick_width' => $options->tickWidth,
-			'start_at_slide' => $options->startSlide,
-			'start_at_end' => $options->startAtEnd,
-		];
+		$preJson['options'] = TimelineOptions::processedParamsToJson( $parameters );
 
 		$json = json_encode( $preJson );
 
@@ -160,24 +79,6 @@ class ModernTimelinePrinter implements ResultPrinter {
 			"if (!window.hasOwnProperty('modernTimeline')) {window.modernTimeline = {};}"
 			. "\n window.modernTimeline.{$timelineId} = $json;"
 		);
-	}
-
-	/**
-	 * @param ProcessedParam[] $parameters
-	 * @return TimelineOptions
-	 */
-	private function newOptionsFromParameters( array $parameters ): TimelineOptions {
-		$options = new TimelineOptions();
-
-		$options->bookmark = $parameters[self::PARAM_BOOKMARK]->getValue();
-		$options->backgroundColor = $parameters[self::PARAM_BACKGROUND]->getValue();
-		$options->scaleFactor = $parameters[self::PARAM_SCALE_FACTOR]->getValue();
-		$options->position = $parameters[self::PARAM_POSITION]->getValue();
-		$options->tickWidth = $parameters[self::PARAM_TICK_WIDTH]->getValue();
-		$options->startSlide = $parameters[self::PARAM_START_SLIDE]->getValue();
-		$options->startAtEnd = $parameters[self::PARAM_START_AT_END]->getValue();
-
-		return $options;
 	}
 
 	public function getQueryMode( $context ): int {
--- src/TimelineOptions.php
+++ src/TimelineOptions.php
@@ -2,14 +2,97 @@
 
 namespace ModernTimeline;
 
+use ParamProcessor\ProcessedParam;
+
 class TimelineOptions {
 
-	public /* bool */ $bookmark;
-	public /* string */ $backgroundColor;
-	public /* int */ $scaleFactor;
-	public /* string */ $position;
-	public /* int */ $tickWidth;
-	public /* int */ $startSlide;
-	public /* bool */ $startAtEnd;
+	public const PARAM_WIDTH = 'width';
+	public const PARAM_HEIGHT = 'height';
+	private const PARAM_BOOKMARK = 'bookmark';
+	private const PARAM_BACKGROUND = 'background';
+	private const PARAM_SCALE_FACTOR = 'scale factor';
+	private const PARAM_POSITION = 'position';
+	private const PARAM_TICK_WIDTH = 'tick width';
+	private const PARAM_START_SLIDE = 'start slide';
+	private const PARAM_START_AT_END = 'start at end';
+
+	public static function getTimelineParameterDefinitions(): array {
+		$definitions = [];
+
+		$definitions[self::PARAM_WIDTH] = [
+			'type' => 'dimension',
+			'allowauto' => true,
+			'units' => [ 'px', 'ex', 'em', '%', '' ],
+			'default' => $GLOBALS['wgModernTimelineWidth'],
+		];
+
+		$definitions[self::PARAM_HEIGHT] = [
+			'type' => 'dimension',
+			'units' => [ 'px', 'ex', 'em', '' ],
+			'default' => $GLOBALS['wgModernTimelineHeight'],
+		];
+
+		$definitions[self::PARAM_BOOKMARK] = [
+			'type' => 'boolean',
+			'default' => $GLOBALS['wgModernTimelineBookmark'],
+		];
+
+		$definitions[self::PARAM_BACKGROUND] = [
+			'type' => 'string',
+			'default' => $GLOBALS['wgModernTimelineBackground'],
+		];
+
+		$definitions[self::PARAM_SCALE_FACTOR] = [
+			'type' => 'integer',
+			'default' => $GLOBALS['wgModernTimelineScaleFactor'],
+		];
+
+		$definitions[self::PARAM_POSITION] = [
+			'type' => 'string',
+			'default' => $GLOBALS['wgModernTimelinePosition'],
+			'values' => [ 'top', 'bottom' ],
+		];
+
+		$definitions[self::PARAM_TICK_WIDTH] = [
+			'type' => 'integer',
+			'default' => $GLOBALS['wgModernTimelineTickWidth']
+		];
+
+		$definitions[self::PARAM_START_SLIDE] = [
+			'type' => 'integer',
+			'default' => $GLOBALS['wgModernTimelineStartSlide']
+		];
+
+		$definitions[self::PARAM_START_AT_END] = [
+			'type' => 'boolean',
+			'default' => $GLOBALS['wgModernTimelineStartAtEnd']
+		];
+
+		foreach ( $definitions as $name => $definition ) {
+			$definitions[$name]['message'] = 'modern-timeline-param-' . str_replace( ' ', '-', $name );
+
+			if ( strpos( $name, ' ' ) !== false ) {
+				$definitions[$name]['aliases'] = [ str_replace( ' ', '', $name ) ];
+			}
+		}
+
+		return $definitions;
+	}
+
+	/**
+	 * @param ProcessedParam[] $parameters
+	 * @return array
+	 */
+	public static function processedParamsToJson( array $parameters ): array {
+		return [
+			'hash_bookmark' => $parameters[self::PARAM_BOOKMARK]->getValue(),
+			'default_bg_color' => $parameters[self::PARAM_BACKGROUND]->getValue(),
+			'scale_factor' => $parameters[self::PARAM_SCALE_FACTOR]->getValue(),
+			'timenav_position' => $parameters[self::PARAM_POSITION]->getValue(),
+			'optimal_tick_width' => $parameters[self::PARAM_TICK_WIDTH]->getValue(),
+			'start_at_slide' => $parameters[self::PARAM_START_SLIDE]->getValue(),
+			'start_at_end' => $parameters[self::PARAM_START_AT_END]->getValue(),
+		];
+	}
 
 }
