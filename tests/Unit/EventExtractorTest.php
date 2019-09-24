53460dd00e587c97bce282f160afe97fe2de82fa
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-25 01:52:51 +0200

    Add to EventExtractorTest

--- tests/Unit/EventExtractorTest.php
+++ tests/Unit/EventExtractorTest.php
@@ -4,19 +4,21 @@ declare( strict_types = 1 );
 
 namespace ModernTimeline\Tests\Unit;
 
+use ModernTimeline\Event;
 use ModernTimeline\EventExtractor;
+use ModernTimeline\ResultFacade\PropertyValueCollection;
 use ModernTimeline\ResultFacade\Subject;
 use ModernTimeline\ResultFacade\SubjectCollection;
 use PHPUnit\Framework\TestCase;
 use SMW\DIWikiPage;
+use SMW\Query\PrintRequest;
+use SMWDITime;
 use Title;
 
 /**
  * @covers \ModernTimeline\EventExtractor
  */
 class EventExtractorTest extends TestCase {
-
-	private const PAGE_NAME = 'Some Page';
 
 	public function testEmptySubjectCollection() {
 		$this->assertExtractsEvents(
@@ -26,13 +28,11 @@ class EventExtractorTest extends TestCase {
 	}
 
 	private function assertExtractsEvents( array $expectedEvents, SubjectCollection $subjects ) {
-		$this->assertSame(
+		$this->assertEquals(
 			$expectedEvents,
 			( new EventExtractor() )->extractEvents( $subjects )
 		);
 	}
-
-
 
 	public function testOnlySubjectsWithNoValues() {
 		$this->assertExtractsEvents(
@@ -48,12 +48,64 @@ class EventExtractorTest extends TestCase {
 		);
 	}
 
-	private function newDiWikiPage( string $pageName = self::PAGE_NAME ): DIWikiPage {
+	private function newDiWikiPage( string $pageName = 'Some page' ): DIWikiPage {
 		$page = $this->createMock( DIWikiPage::class );
 
 		$page->method( 'getTitle' )->willReturn( Title::newFromText( $pageName ) );
 
 		return $page;
+	}
+
+	public function testSingleSubjectWithStartDate() {
+		$subject = new Subject(
+			$this->newDiWikiPage(),
+			[
+				$this->newStartDateValueCollection()
+			]
+		);
+
+		$this->assertExtractsEvents(
+			[
+				new Event(
+					$subject,
+					$this->newStartDate(),
+					null
+				)
+			],
+			new SubjectCollection(
+				[
+					$subject
+				]
+			)
+		);
+	}
+
+	private function newStartDateValueCollection(): PropertyValueCollection {
+		return new PropertyValueCollection(
+			$this->newDatePrintRequestWithLabel( 'Has date' ),
+			[
+				$this->newStartDate()
+			]
+		);
+	}
+
+	private function newDatePrintRequestWithLabel( string $label ): PrintRequest {
+		$pr = $this->createMock( PrintRequest::class );
+		$pr->method( 'getLabel' )->willReturn( $label );
+		$pr->method( 'getTypeID' )->willReturn( '_dat' );
+		return $pr;
+	}
+
+	private function newStartDate(): SMWDITime {
+		return new SMWDITime(
+			SMWDITime::CM_GREGORIAN,
+			2019,
+			8,
+			2,
+			16,
+			7,
+			42
+		);
 	}
 
 }
