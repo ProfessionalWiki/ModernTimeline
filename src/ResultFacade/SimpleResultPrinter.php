37e16eb77bb7ea6972fd7fd869c072b16b2ddaec
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-25 03:28:15 +0200

    Build out new printer registration

--- src/ResultFacade/SimpleResultPrinter.php
+++ src/ResultFacade/SimpleResultPrinter.php
@@ -19,13 +19,61 @@ interface SimpleResultPrinter {
 
 class SimplePrinterRegistry {
 
-	public function register( string $nameMessageKey, array $parameterDefinitions, callable $constructionFunction ) {
+	/**
+	 * @var PrinterInfo[]
+	 */
+	private $printerInfos;
 
+	public function registerPrinter(): SimplePrinterRegistrator {
+		return new SimplePrinterRegistrator( function( PrinterInfo $info ) {
+			$this->printerInfos[] = $info;
+		} );
 	}
 
 }
 
-class SimplePrinterInfo {
+class SimplePrinterRegistrator {
+
+	private $registry;
+
+	private $nameMessageKey;
+	private $parameterDefinitions;
+	private $constructionFunction;
+
+	public function __construct( callable $registry ) {
+		$this->registry = $registry;
+	}
+
+	public function withMessageKey( string $nameMessageKey ): self {
+		$this->nameMessageKey = $nameMessageKey;
+		return $this;
+	}
+
+	public function andParameterDefinitions( array $parameterDefinitions ): self {
+		$this->parameterDefinitions = $parameterDefinitions;
+		return $this;
+	}
+
+	public function andConstructionFunction( callable $constructionFunction ): self {
+		$this->constructionFunction = $constructionFunction;
+		return $this;
+	}
+
+	public function register() {
+		call_user_func( $this->registry, $this->newPrinterInfo() );
+	}
+
+	private function newPrinterInfo(): PrinterInfo {
+		return new PrinterInfo(
+			$this->nameMessageKey,
+			$this->parameterDefinitions,
+			$this->constructionFunction
+		);
+	}
+
+}
+
+class PrinterInfo {
 
 	private $nameMessageKey;
 	private $parameterDefinitions;
