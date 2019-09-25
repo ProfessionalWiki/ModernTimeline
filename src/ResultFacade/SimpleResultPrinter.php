4dffe3a8ea35a78e67220b79c324838b58e0a275
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-25 03:06:40 +0200

    Add SimplePrinterRegistry stub

--- src/ResultFacade/SimpleResultPrinter.php
+++ src/ResultFacade/SimpleResultPrinter.php
@@ -14,3 +14,39 @@ interface SimpleResultPrinter {
 	public function getResult( SimpleQueryResult $result ): string;
 
 }
+
+
+
+class SimplePrinterRegistry {
+
+	public function register( string $nameMessageKey, array $parameterDefinitions, callable $constructionFunction ) {
+
+	}
+
+}
+
+class SimplePrinterInfo {
+
+	private $nameMessageKey;
+	private $parameterDefinitions;
+	private $constructionFunction;
+
+	public function __construct( string $nameMessageKey, array $parameterDefinitions, callable $constructionFunction ) {
+		$this->nameMessageKey = $nameMessageKey;
+		$this->parameterDefinitions = $parameterDefinitions;
+		$this->constructionFunction = $constructionFunction;
+	}
+
+	public function getNameMessageKey(): string {
+		return $this->nameMessageKey;
+	}
+
+	public function getParameterDefinitions(): array {
+		return $this->parameterDefinitions;
+	}
+
+	public function buildPrinter(): SimpleResultPrinter {
+		return call_user_func( $this->constructionFunction );
+	}
+
+}
